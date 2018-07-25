52da907ff56a8417776bfb7d4fd3aa45289eab7b
Raphaël Berly <raphaelberly@gmail.com>
2018-07-25 19:58:46 +0200

    Added new bulks

--- main_bulk.py
+++ main_bulk.py
@@ -1,5 +1,6 @@
 
 import argparse
+import yaml
 from lib.bulk import Bulk
 
 # Create argument parser
@@ -12,7 +13,7 @@ target.add_argument('--types', '-t', nargs='*')
 args = parser.parse_args()
 
 # Generate target type list
-type_list = ['ratings', 'titles']
+type_list = yaml.load(open('config/bulk.yaml')).keys()
 target_types = type_list if args.all else args.types
 
 # For each target type, run the ETL process
