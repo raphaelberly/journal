75594a26f0ea63ecb27c532d2d62c253a81eb193
Raphaël Berly <raphaelberly@gmail.com>
2018-09-13 15:25:51 +0200

    Cleaned old unused files and pieces of code

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,6 @@ from app import app
 from app import db
 from datetime import datetime, date
 from flask import render_template, redirect, url_for, session, request
-from app.forms import *
 from app.models import Record, Title
 from lib.search import Search
 
@@ -19,90 +18,6 @@ def get_post_result(key):
 @app.context_processor
 def inject_now():
     return {'now': datetime.now()}
-
-
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/search', methods=['GET', 'POST'])
-def search():
-    # Instantiate page forms
-    searchForm = SearchForm()
-    moreResultForm = MoreResultForm()
-    # If searchForm is submitted
-    if searchForm.validate_on_submit():
-        # Get first movie result based on search input
-        results = Search(searchForm.input.data, 'config').get_results()
-        if results:
-            session['results'] = results
-            session['ids'] = [result['id'] for result in session['results']]
-            session['movies'] = [session['results'][0]]
-            # Render page
-            return render_template('search.html', mode='single', searchForm=searchForm, singleResultForm=moreResultForm)
-        else:
-            session['results'] = None
-            session['ids'] = None
-            session['movies'] = None
-            return render_template('search.html', mode='none', searchForm=searchForm)
-    if moreResultForm.validate_on_submit():
-        # If moreButton was clicked
-        if moreResultForm.moreButton.data:
-            # Add the next movie results based on search input
-            session['movies'] = session['results']
-            # Render page
-            return render_template('search.html', mode='multiple', searchForm=searchForm)
-    # Else, render a page with only the searchForm
-    return render_template('search.html', searchForm=searchForm)
-
-
-@app.route('/<movie_id>/add', methods=['GET', 'POST'])
-def add(movie_id):
-    # Check movie was found through search
-    if movie_id not in session['ids']:
-        return redirect(url_for('search'))
-    movie = [item for item in session['movies'] if item['id'] == movie_id][0]
-    # Instantiate page forms
-    searchForm = SearchForm()
-    addForm = AddForm()
-    # Render page
-    return render_template('add.html', addForm=addForm, searchForm=searchForm, movie=movie)
-
-
-@app.route('/<movie_id>/response', methods=['GET', 'POST'])
-def respond(movie_id):
-    # Instantiate forms
-    searchForm = SearchForm()
-    resultsForm = ResultsForm()
-    # Check movie was found through search
-    if movie_id not in session['ids']:
-        return redirect(url_for('search'))
-    # If there was a post request
-    if request.method == 'POST':
-        # If a grade was submitted
-        if 'gradeRange' in request.form:
-            # If movie already in db, return 'existed' response page
-            record = Record.query.filter_by(movie=movie_id).first()
-            if record:
-                return render_template('response.html', resultsForm=resultsForm, searchForm=searchForm,
-                                       movie_id=movie_id, grade=record.grade, response='existed')
-            # Else, add movie to db and return 'added' response page
-            grade = float(dict(request.form)['gradeRange'][0])
-            # Add the movie to the database
-            record = Record(movie=movie_id, grade=grade)
-            db.session.add(record)
-            db.session.commit()
-            # Render response template
-            return render_template('response.html', resultsForm=resultsForm, searchForm=searchForm, movie_id=movie_id,
-                                   grade=grade, response='added')
-        # If cancel button was clicked
-        if 'cancelButton' in request.form:
-            # Remove movie from db
-            to_delete = Record.query.filter_by(movie=movie_id).all()
-            for record in to_delete:
-                db.session.delete(record)
-            db.session.commit()
-            # Render 'cancelled' response page
-            return render_template('response.html', searchForm=searchForm, movie_id=movie_id, response='cancelled')
-    # Else, redirect to search page
-    return redirect(url_for('search'))
 
 
 @app.route('/recent', methods=['GET'])
@@ -139,8 +54,9 @@ def statistics(nb_movies=3):
     return render_template('statistics.html', title='Statistics', metrics=metrics, best_movies=best_movies)
 
 
-@app.route('/search2', methods=['GET', 'POST'])
-def search2():
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/search', methods=['GET', 'POST'])
+def search():
 
     if request.method == 'POST':
         if 'input' in request.form:
@@ -153,12 +69,12 @@ def search2():
             # session['records'] = records
 
             LOGGER.info('SETTING INPUT TO {}'.format(input))
-            return render_template('search2.html', title='Search', results=results, records=records)
+            return render_template('search.html', title='Search', results=results, records=records)
 
     LOGGER.info('SETTING INPUT TO NONE')
     session['input'] = None
     # session['results'] = None
-    return render_template('search2.html', title='Search')
+    return render_template('search.html', title='Search')
 
 
 @app.route('/movie/<movie_id>', methods=['GET', 'POST'])
