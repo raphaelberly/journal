4a6619630cb0ad69e42fe39cfca417823b0e5854
Raphaël Berly <raphaelberly@gmail.com>
2018-09-16 12:26:54 +0200

    Added 'DDL' folder (notably with materialized views queries for Directors, Writers and Genres)

---
+++ ddl/materialized_views/directors.sql
@@ -0,0 +1,58 @@
+CREATE MATERIALIZED VIEW journal.directors AS (
+
+WITH
+
+movies AS (
+
+  SELECT
+    v.movie,
+    v.grade,
+    r.rating,
+    v.date,
+    t.title,
+    unnest(string_to_array(c.directors, ',')) AS director
+  FROM journal.crew c
+  INNER JOIN journal.views v
+    ON c.movie = v.movie
+  INNER JOIN journal.ratings r
+    ON c.movie = r.movie
+  INNER JOIN journal.titles t
+    ON c.movie = t.movie
+
+),
+
+movies_with_director_name AS (
+
+  SELECT
+    m.*,
+    n.name
+  FROM movies m
+  INNER JOIN journal.names n
+    ON m.director = n.person
+
+),
+
+directors AS (
+
+  SELECT
+    m.director,
+    m.name,
+    array_agg(m.title ORDER BY m.grade DESC, m.rating DESC) AS titles,
+    avg(m.grade)  AS grade,
+    avg(m.rating) AS rating,
+    count(*)      AS count
+  FROM movies_with_director_name m
+  GROUP BY 1,2
+
+)
+
+SELECT
+  d.director AS id,
+  d.name,
+  d.titles[1:3] AS top_3,
+  d.grade,
+  d.rating,
+  d.count
+FROM directors d
+
+);
---
+++ ddl/materialized_views/genres.sql
@@ -0,0 +1,43 @@
+CREATE MATERIALIZED VIEW journal.genres AS (
+
+WITH
+
+movies_with_genre AS (
+
+  SELECT
+    unnest(string_to_array(t.genres, ',')) AS genre,
+    v.movie,
+    v.grade,
+    r.rating,
+    v.date,
+    t.title
+  FROM journal.views v
+  INNER JOIN journal.ratings r
+    ON v.movie = r.movie
+  INNER JOIN journal.titles t
+    ON v.movie = t.movie
+
+),
+
+genres AS (
+
+  SELECT
+    m.genre,
+    array_agg(m.title ORDER BY m.grade DESC, m.rating DESC) AS titles,
+    avg(m.grade)  AS grade,
+    avg(m.rating) AS rating,
+    count(*)      AS count
+  FROM movies_with_genre m
+  GROUP BY 1
+
+)
+
+SELECT
+  g.genre       AS name,
+  g.titles[1:3] AS top_3,
+  g.grade,
+  g.rating,
+  g.count
+FROM genres g
+
+);
