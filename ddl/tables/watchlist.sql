bda6dc617518631b8f7db3d9fbc0e9bb7103173b
Raphaël Berly <raphaelberly@gmail.com>
2018-12-28 14:27:49 +0100

    Added watchlist Postgres table creation script

---
+++ ddl/tables/watchlist.sql
@@ -0,0 +1,12 @@
+CREATE TABLE journal.watchlist(
+
+  insert_datetime TIMESTAMP DEFAULT NOW(),
+  movie VARCHAR(9) NOT NULL PRIMARY KEY,
+  title TEXT NOT NULL,
+  year INTEGER NOT NULL,
+  genres TEXT NOT NULL,
+  directors TEXT[] NOT NULL,
+  "cast" TEXT[] NOT NULL,
+  image VARCHAR(1024)
+
+);
