ab50b0d2231d94135f0bc789b84e1e60e89958ea
Raphaël Berly <raphaelberly@gmail.com>
2019-01-04 10:32:44 +0100

    Add "User" model and table creation script

--- app/models.py
+++ app/models.py
@@ -1,6 +1,33 @@
-
-from app import db
 from datetime import datetime
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from app import db, login
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class User(UserMixin, db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30))
+    password_hash = db.Column(db.String(100))
+    email = db.Column(db.String(256))
+
+    __tablename__ = "users"
+    __table_args__ = {"schema": "journal"}
+
+    def __repr__(self):
+        return '<User {0}: {1}>'.format(self.id, self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Title(db.Model):
---
+++ ddl/tables/users.sql
@@ -0,0 +1,9 @@
+CREATE TABLE journal.users (
+
+  insert_datetime TIMESTAMP DEFAULT now(),
+  id SERIAL NOT NULL CONSTRAINT users_pkey PRIMARY KEY,
+  username VARCHAR(20) NOT NULL,
+  password_hash VARCHAR(100) NOT NULL,
+  email TEXT NOT NULL
+
+);
