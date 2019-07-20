d57b7caab098e60da4ce6da1ad2b9d80c3c4d3a8
Raphael Berly <raphaelberly@gmail.com>
2019-07-20 16:39:11 +0200

    Stop using base.html and outsource js scripts in Movie page

---
+++ app/static/js/grade.js
@@ -0,0 +1,8 @@
+var slider = document.getElementById("gradeRange");
+var output = document.getElementById("gradeValue");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+    <!-- Set the toFixed value to 1 to have a fixed 1-decimal printed value -->
+    output.innerHTML = parseFloat(this.value).toFixed(0);
+}
