7052b328c9f3591588eff13d03f8b1ab6f832a01
Raphael Berly <raphaelberly@gmail.com>
2019-07-23 22:24:52 +0200

    Split "add_referrer" into "add_referrer" and "add_referrer_and_load"

--- app/static/js/referrer.js
+++ app/static/js/referrer.js
@@ -1,5 +1,9 @@
 add_referrer = function(url, current_title) {
-    if (url.includes('?'))  { var conn = '&' } else { var conn = '?' }
-    var new_url = url.concat(conn, 'ref=', current_title, '&ref_scroll=', window.scrollY)
-    window.location.href = new_url
+    if (url.includes('?'))  { var conn = '&' } else { var conn = '?' };
+    var new_url = url.concat(conn, 'ref=', current_title, '&ref_scroll=', window.scrollY);
+    return new_url;
+};
+
+add_referrer_and_load = function(url, current_title) {
+    window.location.href = add_referrer(url, current_title);
 };
