0142dc3f12f857695940382082cda4ef5a79f6fa
Raphael Berly <raphaelberly@gmail.com>
2019-07-23 19:58:39 +0200

    Modify alert speed and duration parameters

--- app/static/js/alert.js
+++ app/static/js/alert.js
@@ -1,7 +1,7 @@
 setTimeout(function(){
     document.getElementsByClassName('alert hidden')[0].className = 'alert visible';
-}, 100);
+}, 0);
 
 setTimeout(function(){
     document.getElementsByClassName('alert visible')[0].className = 'alert hidden';
-}, 4100);
+}, 3500);
