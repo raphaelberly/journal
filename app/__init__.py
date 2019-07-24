c854a8a37659be31f962d4d0c6308ecff10c6e45
Raphael Berly <raphaelberly@gmail.com>
2019-07-24 20:49:35 +0200

    Added automatic cache buster for .js, .css, and .json files

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,22 @@
 
+from config.app import Config
 from flask import Flask
 from flask_login import LoginManager
-from config.app import Config
+from flask_cachebuster import CacheBuster
 from flask_sqlalchemy import SQLAlchemy
 
+# Initialise app
 app = Flask(__name__)
 app.config.from_object(Config)
+
+# Initialise DB
 db = SQLAlchemy(app)
+
+# Initialise login manager
 login = LoginManager(app)
+
+# Create cache buster
+cache_buster = CacheBuster(config={'extensions': ['.js', '.css', '.json'], 'hash_size': 5})
+cache_buster.init_app(app)
 
 from app import routes, models
