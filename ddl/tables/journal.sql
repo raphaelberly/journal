16cb56d1ac6730509b0f3905e2f69c8bea7657ea
Raphael Berly <raphaelberly@gmail.com>
2020-05-08 17:00:02 +0200

    Refactor journal DDL files into a single one

---
+++ ddl/tables/journal.sql
@@ -0,0 +1,78 @@
+CREATE SCHEMA imdb;
+
+CREATE TABLE imdb.titles (
+  id                    INTEGER         NOT NULL,
+  imdb_id               VARCHAR(32)     NOT NULL UNIQUE,
+  title                 TEXT            NOT NULL,
+  release_date          DATE            NOT NULL,
+  original_title        TEXT            NOT NULL,
+  original_language     VARCHAR(32)     NOT NULL,
+  directors             VARCHAR(256)[],
+  genres                VARCHAR(256)[],
+  top_cast              VARCHAR(256)[],
+  poster_path           TEXT,
+  runtime               SMALLINT,
+  revenue               BIGINT,
+  budget                BIGINT,
+  tagline               TEXT,
+  insert_datetime_utc   TIMESTAMP       NOT NULL DEFAULT (now() AT TIME ZONE 'utc'),
+  update_datetime_utc   TIMESTAMP       NOT NULL DEFAULT (now() AT TIME ZONE 'utc'),
+  CONSTRAINT "titles_pkey" PRIMARY KEY (id)
+);
+
+CREATE TABLE imdb.persons (
+  id                    INTEGER         NOT NULL,
+  name                  VARCHAR(128)    NOT NULL,
+  gender                SMALLINT        NULL,
+  insert_datetime_utc   TIMESTAMP       NOT NULL DEFAULT (now() AT TIME ZONE 'utc'),
+  update_datetime_utc   TIMESTAMP       NOT NULL DEFAULT (now() AT TIME ZONE 'utc'),
+  CONSTRAINT "persons_pkey" PRIMARY KEY (id)
+);
+
+CREATE TABLE imdb.credits (
+  id                    VARCHAR(128)    NOT NULL,
+  tmdb_id               INTEGER         NOT NULL,
+  person_id             INTEGER         NOT NULL,
+  role                  VARCHAR(128)    NOT NULL,
+  cast_rank             SMALLINT        NULL,
+  insert_datetime_utc   TIMESTAMP       NOT NULL DEFAULT (now() AT TIME ZONE 'utc'),
+  update_datetime_utc   TIMESTAMP       NOT NULL DEFAULT (now() AT TIME ZONE 'utc'),
+  CONSTRAINT "credits_pkey" PRIMARY KEY (id),
+  CONSTRAINT "credits_fkey_titles" FOREIGN KEY (tmdb_id) REFERENCES imdb.titles (id),
+  CONSTRAINT "credits_fkey_persons" FOREIGN KEY (person_id) REFERENCES imdb.persons (id)
+);
+
+CREATE TABLE imdb.users (
+  id                    SERIAL          NOT NULL,
+  username              VARCHAR(32)     NOT NULL UNIQUE,
+  password_hash         VARCHAR(128)    NOT NULL,
+  email                 VARCHAR(256)    NOT NULL,
+  grade_as_int          BOOLEAN         NOT NULL DEFAULT TRUE,
+  insert_datetime_utc   TIMESTAMP       NOT NULL DEFAULT (now() AT TIME ZONE 'utc'),
+  update_datetime_utc   TIMESTAMP       NOT NULL DEFAULT (now() AT TIME ZONE 'utc'),
+  CONSTRAINT "users_pkey" PRIMARY KEY (id)
+);
+
+CREATE TABLE imdb.watchlist(
+  user_id               INTEGER         NOT NULL,
+  tmdb_id               INTEGER         NOT NULL,
+  providers             VARCHAR(64)[]   NOT NULL,
+  insert_datetime_utc   TIMESTAMP       NOT NULL DEFAULT (now() AT TIME ZONE 'utc'),
+  update_datetime_utc   TIMESTAMP       NOT NULL DEFAULT (now() AT TIME ZONE 'utc'),
+  CONSTRAINT "watchlist_pkey" PRIMARY KEY (user_id,tmdb_id),
+  CONSTRAINT "watchlist_fkey_users" FOREIGN KEY (user_id) REFERENCES imdb.users (id),
+  CONSTRAINT "watchlist_fkey_titles" FOREIGN KEY (tmdb_id) REFERENCES imdb.titles (id)
+);
+
+CREATE TABLE imdb.records(
+  user_id               INTEGER             NOT NULL,
+  tmdb_id               INTEGER             NOT NULL,
+  grade                 DOUBLE PRECISION    NOT NULL,
+  date                  DATE                NOT NULL DEFAULT DATE(now() AT TIME ZONE 'utc'),
+  recent                BOOLEAN             NOT NULL DEFAULT TRUE,
+  insert_datetime_utc   TIMESTAMP           NOT NULL DEFAULT (now() AT TIME ZONE 'utc'),
+  update_datetime_utc   TIMESTAMP           NOT NULL DEFAULT (now() AT TIME ZONE 'utc'),
+  CONSTRAINT "records_pkey" PRIMARY KEY (user_id,tmdb_id),
+  CONSTRAINT "records_fkey_users" FOREIGN KEY (user_id) REFERENCES imdb.users (id),
+  CONSTRAINT "records_fkey_titles" FOREIGN KEY (tmdb_id) REFERENCES imdb.titles (id)
+);
