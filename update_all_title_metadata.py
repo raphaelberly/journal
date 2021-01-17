fe601ecc094a491aebc4ba719928cc396f314ce6
Raphael Berly <raphaelberly@gmail.com>
2021-01-17 23:17:46 +0100

    Add error handling to update_all_title_metadata.py script

--- update_all_title_metadata.py
+++ update_all_title_metadata.py
@@ -15,10 +15,18 @@ tmdb = Tmdb()
 title_ids = [title_id for title_id, in db.session.query(Title.id).all()]
 
 # Update all titles one after the other
+errors = []
 for title_id in tqdm(title_ids):
     title = tmdb.get(title_id)
-    upsert_title_metadata(item=title)
+    try:
+        upsert_title_metadata(item=title)
+    except Exception:
+        errors.append(errors)
     sleep(0.2)
 
 # Commit changes if everything went well
 db.session.commit()
+
+# Log errors if any
+if errors:
+    print(f'Could not update titles: {errors}')
