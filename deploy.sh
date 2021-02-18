f1399ce485071c956754a04f79615e62a7cd70a8
Raphael Berly <raphaelberly@gmail.com>
2021-02-18 12:18:41 +0100

    Make deploy.sh more generic

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ echo "### Stopping server..."
 sudo service supervisor stop
 
 echo "### Pulling changes from pi-hub..."
-git pull pi-hub master
+git pull origin master
 
 echo "### Starting server..."
 sudo service supervisor start
