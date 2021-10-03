a20b9ffbf01e60b6d5548831e25830623de74d87
Raphael Berly <raphaelberly@gmail.com>
2021-10-03 12:13:52 +0200

    Add OMDb support to collect movie ratings

---
+++ lib/omdb.py
@@ -0,0 +1,29 @@
+import json
+import os
+from functools import lru_cache
+
+import requests
+
+from lib.tools import read_config
+
+
+class Omdb(object):
+
+    URL_MOVIE = 'http://www.omdbapi.com/?apikey={api_key}&i={title_id}'
+
+    def __init__(self, config_path: str = 'config'):
+        credentials = read_config(os.path.join(config_path, 'credentials.yaml'))
+        self._api_key = credentials['omdb']['api_key']
+
+    @lru_cache(96)
+    def _get(self, title_id: str) -> dict:
+        url = self.URL_MOVIE.format(api_key=self._api_key, title_id=title_id)
+        response = requests.get(url)
+        if response.status_code == 200:
+            response = json.loads(response.content)
+            if response['Response'] == 'True':
+                return response
+        return {}
+
+    def imdb_rating(self, title_id: str) -> str:
+        return self._get(title_id).get('imdbRating')
