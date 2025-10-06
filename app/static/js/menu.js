ee6979d622e3f4a9788bf108cc5ca421f08e7705
Raphaël Berly <raphaelberly@gmail.com>
2025-10-06 18:49:18 +0200

    Adjust side-navigation menu's animation

--- app/static/js/menu.js
+++ app/static/js/menu.js
@@ -1,12 +1,12 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.right = "0";
     document.getElementById("sidenavback").style.transition = "background-color .2s linear 0s, z-index 0s linear 0s";
     document.getElementById("sidenavback").style.zIndex = "3";
     document.getElementById("sidenavback").style.backgroundColor = "rgba(0,0,0,0.3)";
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.right = "-250px";
     document.getElementById("sidenavback").style.transition = "background-color .2s linear 0s, z-index 0s linear .2s";
     document.getElementById("sidenavback").style.backgroundColor = "rgba(0,0,0,0.0)";
     document.getElementById("sidenavback").style.zIndex = "-1";
